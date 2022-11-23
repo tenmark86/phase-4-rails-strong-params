d2f5b0e78536bac6fab9308817bf905f1a070e10
tenmark86e <tenmark86@gmail.com>
2022-11-23 16:38:35 +0300

    Done Strong Params and Mass Assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,4 +22,10 @@ class BirdsController < ApplicationController
     end
   end
 
+  private
+  # all methods below here are private
+  def bird_params
+    params.permit(:name, :species)
+  end
+
 end
